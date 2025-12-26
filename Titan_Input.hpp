29d7e2f2ea4357badd8f1b80785f981125b0ed9d
TeaDev <129286811+DreamModule@users.noreply.github.com>
2025-12-26 21:15:13 +0200

    Update Titan_Input.hpp

--- Titan_Input.hpp
+++ Titan_Input.hpp
@@ -1,18 +1,29 @@
-#pragma once
+#ifndef TITAN_INPUT_HPP
+#define TITAN_INPUT_HPP
 #include "Titan_Core.hpp"
-
 namespace Titan {
-
-enum class KeyCode : u32 {
-    Unknown = 0
-};
-
-struct InputManager {
-    void Init();
-    void Update();
-    bool GetAction(const char*) const;
-    bool GetActionDown(const char*) const;
-    bool GetActionUp(const char*) const;
-    f32 GetAxis(const char*) const;
-};
+    enum class KeyCode : u32 { 
+        Unknown=0, W, A, S, D, Space, Escape, Enter, MouseLeft, MouseRight 
+    };
+    
+    struct InputManager {
+        void Init();
+        void Update();
+        void MapAction(const char* n, KeyCode k);
+        void MapAxis(const char* n, KeyCode pos, KeyCode neg);
+        
+        bool GetAction(const char* n) const;
+        bool GetActionDown(const char* n) const;
+        f32 GetAxis(const char* n) const;
+        
+        // Injection (Joystick)
+        void InjectAxis(const char* n, f32 val);
+        
+        // Multi-touch / Pointers
+        static const u32 MAX_POINTERS = 10;
+        Math::Vec2 GetPointerPosition(u32 id) const;
+        bool GetPointerDown(u32 id) const;
+        bool GetPointerUp(u32 id) const;
+    };
 }
+#endif
