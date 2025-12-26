2e88bc5b15c5d72d0c816355528289f29b604cd8
TeaDev <129286811+DreamModule@users.noreply.github.com>
2025-12-26 21:20:16 +0200

    Update Titan_Window.hpp

--- Titan_Window.hpp
+++ Titan_Window.hpp
@@ -1,14 +1,5 @@
 #ifndef TITAN_WINDOW_HPP
 #define TITAN_WINDOW_HPP
 #include "Titan_Core.hpp"
-namespace Titan {
-struct Window {
-    struct System {
-        static void Init(u32 w, u32 h, const char* t);
-        static bool Update();
-        static void SwapBuffers();
-        static void Shutdown();
-    };
-};
-}
+namespace Titan { struct Window { struct System { static void Init(u32 w, u32 h, const char* t); static bool Update(); static void SwapBuffers(); static void Shutdown(); }; }; }
 #endif
