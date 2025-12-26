15d112fa7d197786deffb7a6ee53ec86d48fdd9e
TeaDev <129286811+DreamModule@users.noreply.github.com>
2025-12-26 19:22:29 +0200

    Update Titan_Input.hpp

--- Titan_Input.hpp
+++ Titan_Input.hpp
@@ -1,33 +1,18 @@
-#ifndef TITAN_INPUT_HPP
-#define TITAN_INPUT_HPP
-
+#pragma once
 #include "Titan_Core.hpp"
 
 namespace Titan {
 
-enum class KeyCode : u16 {
-    None=0, W, A, S, D, Space, Escape, Shift, Ctrl, Enter,
-    Up, Down, Left, Right,
-    MouseLeft, MouseRight, MouseMiddle,
-    MouseX, MouseY, MouseScroll,
-    GamepadLeftStickX, GamepadLeftStickY, GamepadFaceButtonBottom,
-    VirtualStickX, VirtualStickY, Count
+enum class KeyCode : u32 {
+    Unknown = 0
 };
 
-struct Input {
-    struct Manager {
-        static void Init();
-        static void MapAction(const char* n, KeyCode k);
-        static void MapAxisKeys(const char* n, KeyCode p, KeyCode ng);
-        static void MapAxisDirect(const char* n, KeyCode a, f32 s=1.0f);
-        static void Update();
-        static bool GetActionDown(const char* n);
-        static bool GetActionUp(const char* n);
-        static bool GetAction(const char* n);
-        static f32  GetAxis(const char* n);
-        static void InjectAxis(const char* n, f32 v);
-        static Math::Vec2 GetMousePosition();
-    };
+struct InputManager {
+    void Init();
+    void Update();
+    bool GetAction(const char*) const;
+    bool GetActionDown(const char*) const;
+    bool GetActionUp(const char*) const;
+    f32 GetAxis(const char*) const;
 };
 }
-#endif
