4d92a8bdea76b98d22a87bd2d9e343d1c1e6743c
TeaDev <129286811+DreamModule@users.noreply.github.com>
2025-12-27 02:10:33 +0200

    Update InputSystem.cpp

--- src/Core/Systems/InputSystem.cpp
+++ src/Core/Systems/InputSystem.cpp
@@ -1,9 +1,11 @@
 #include "InputSystem.hpp"
+#include "../../Input/Joystick.hpp"
 
 namespace Titan::Core::Systems {
 
 void InputSystem::Update(Titan::Core::FrameContext& ctx) {
     (void)ctx;
+    Titan::Input::Joystick::Update();
 }
 
 }
