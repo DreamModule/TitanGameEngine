26c674cdb467e52a5fc7c36be1a0eb8ce14a98a6
TeaDev <129286811+DreamModule@users.noreply.github.com>
2025-12-28 15:50:45 +0200

    Update World.hpp

--- src/ECS/World.hpp
+++ src/ECS/World.hpp
@@ -1,23 +1,106 @@
 #pragma once
-#include "Entity.hpp"
-#include "Components/Transform.hpp"
-#include "Components/Sprite.hpp"
-#include <unordered_map>
+
+#include “Entity.hpp”
+#include “ComponentManager.hpp”
+#include “System.hpp”
 #include <vector>
-#include <cstdint>
+#include <queue>
+#include <memory>
+#include <algorithm>
 
-namespace Titan::ECS {
+namespace Titan {
+namespace ECS {
 
-struct World {
-    Entity CreateEntity();
-    void DestroyEntity(Entity e);
-    std::vector<Entity> GetAllEntities() const;
+class World {
+public:
+World();
+~World();
 
-    std::unordered_map<Entity, Components::Transform> transforms;
-    std::unordered_map<Entity, Components::Sprite> sprites;
+```
+Entity CreateEntity();
+void DestroyEntity(Entity entity);
+bool IsEntityValid(Entity entity) const;
+
+template<typename T>
+void RegisterComponent() {
+    componentManager.RegisterComponent<T>();
+}
+
+template<typename T>
+void AddComponent(Entity entity, const T& component) {
+    if (!IsEntityValid(entity)) return;
+    componentManager.AddComponent<T>(entity, component);
+}
+
+template<typename T>
+void RemoveComponent(Entity entity) {
+    if (!IsEntityValid(entity)) return;
+    componentManager.RemoveComponent<T>(entity);
+}
+
+template<typename T>
+T& GetComponent(Entity entity) {
+    return componentManager.GetComponent<T>(entity);
+}
+
+template<typename T>
+const T& GetComponent(Entity entity) const {
+    return componentManager.GetComponent<T>(entity);
+}
+
+template<typename T>
+bool HasComponent(Entity entity) const {
+    return componentManager.HasComponent<T>(entity);
+}
+
+template<typename T>
+std::shared_ptr<ComponentArray<T>> GetComponentArray() {
+    return componentManager.GetComponentArray<T>();
+}
+
+template<typename T>
+std::shared_ptr<const ComponentArray<T>> GetComponentArray() const {
+    return componentManager.GetComponentArray<T>();
+}
+
+void RegisterSystem(std::shared_ptr<ISystem> system, int priority = 0);
+void UnregisterSystem(std::shared_ptr<ISystem> system);
+
+void Update(float deltaTime);
+void FixedUpdate(float fixedDeltaTime);
+void Render();
+
+size_t GetEntityCount() const;
+const std::vector<Entity>& GetEntities() const;
+
+void Clear();
+```
 
 private:
-    Entity nextId = 1;
+ComponentManager componentManager;
+
+```
+std::vector<Entity> entities;
+std::queue<uint32_t> freeIndices;
+std::vector<uint32_t> generations;
+
+struct SystemEntry {
+    std::shared_ptr<ISystem> system;
+    int priority;
+    
+    bool operator<(const SystemEntry& other) const {
+        return priority < other.priority;
+    }
 };
 
+std::vector<SystemEntry> systems;
+
+uint32_t nextIndex;
+
+void SortSystems();
+```
+
+};
+
+}
 }
